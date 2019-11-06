07d5db33dcc6576a7f64e10de44ef698ba4410dc
Nil <45631804+Fedakope@users.noreply.github.com>
2019-11-06 11:50:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    $(function(){ $("#navbar").load("components/navbar.html") })  
+    $(function(){ $("#jumbotron").load("components/jumbotron.html") }) 
+    $(function(){ $("#footer").load("components/footer.html") }) 
+  });
+
+  
